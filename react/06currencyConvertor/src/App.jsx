ef3f2f2d066fc7478ecaa6bbfe1e19a0b5327c68
Lonewolf <yatharthbhat2004@gmail.com>
2024-04-02 18:11:57 +0530

    some changes in currency convertor and react router

--- react/06currencyConvertor/src/App.jsx
+++ react/06currencyConvertor/src/App.jsx
@@ -47,7 +47,7 @@ function App() {
                 label="From"
                 amount={amount}
                 currencyOptions={options}
-                onCurrencyChange={(currency) => setAmount(amount)}
+                onCurrencyChange={(currency) => setAmount(currency)}
                 selectCurrency={from}
                 onAmountChange={(amount) => setAmount(amount)}
               />
