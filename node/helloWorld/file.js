0bf0c43cd01e009b1934c492b47d603cda62cb96
Lonewolf <yatharthbhat2004@gmail.com>
2024-05-19 23:20:02 +0530

    commit

---
+++ node/helloWorld/file.js
@@ -0,0 +1,2 @@
+const fs = require("fs");
+fs.writeFileSync("./math2.js","function add(a,b){return a+b;}")
---
+++ node/helloWorld/helloWorld.js
@@ -0,0 +1,3 @@
+const math=require("./math.js")
+console.log("Hey there");
+console.log(math.add(8,9));
