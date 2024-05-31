644e6fb0bfa0fc4c0b6d508d4da86fb41370a6b0
Yatharth Bhatnagar <122897945+YatharthBhatnagar2004@users.noreply.github.com>
2024-05-31 18:17:07 +0530

    Delete WebDev directory

--- WebDev/js cource/javascript-amazon-project/scripts/checkout.js
+++
@@ -1,6 +0,0 @@
-import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
-import { renderOrderSummary } from "./checkout/orderSummary.js";
-import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-renderOrderSummary();
-renderPaymentSummary();
-renderCheckoutHeader();
